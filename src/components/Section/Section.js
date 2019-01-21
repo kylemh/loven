9d2426dcf17be72ac09bf67c3f3f706debcacf5e
Kyle Holmberg <kylemh.email@gmail.com>
2019-01-21 10:33:25 +0100

    Create <Section> component

---
+++ src/components/Section/Section.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Section.module.scss';
+
+Section.propTypes = {
+  columns: PropTypes.array.isRequired, // can be JSX, elements, or strings
+  id: PropTypes.string,
+  title: PropTypes.string,
+};
+
+Section.defaultProps = {
+  id: undefined,
+  title: undefined,
+};
+
+function Section({ columns, id, title }) {
+  return (
+    <section id={id}>
+      <h3 className={styles.title}>{title}</h3>
+
+      <div className={styles.columnsContainer}>
+        {columns.map((column, index) =>
+          React.cloneElement(column, { key: index })
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Section;
