3d4f76d3141f7f01f46c56bd5a8b734da3f3071c
Kyle Holmberg <kylemh.email@gmail.com>
2019-01-22 23:17:20 +0100

    Add basis content to /subscribe

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Container from '../components/Container';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
-const SecondPage = () => (
+const Subscribe = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Subscribe To Bakery Boxes" />
+
+    <Container isFullViewportHeight>
+      <h1>Subscribe To Bakery Boxes</h1>
+
+      {/* <p>
+        Being more than just a bakery also extends to our product. We don't make
+        the goods until you buy in - nothing is sitting on a window sill for
+        hours. We deliver it right to your home immediately after baking it. The
+        catch is that we only deliver once a month, on a non-refundable 12-month
+        subscription basis.
+      </p> */}
+
+      <Link to="/">Go back to the homepage</Link>
+    </Container>
   </Layout>
 );
 
-export default SecondPage;
+export default Subscribe;
