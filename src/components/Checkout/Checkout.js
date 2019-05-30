03a3db885288c4042c7a5780a0062510556f4398
Kyle Holmberg <kylemh.email@gmail.com>
2019-05-29 23:36:05 -0700

    Use live product and remove beta config

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -3,13 +3,21 @@ import { Button } from '../Button';
 
 class Checkout extends Component {
   componentDidMount() {
-    this.stripe = window.Stripe('pk_test_i27dZgga81biOwh7ggQtOg1W', {
-      betas: ['checkout_beta_4'],
-    });
+    console.log(process.env.NODE_ENV);
+    this.stripe = window.Stripe(process.env.GATSBY_STRIPE_PUBLIC_KEY);
   }
 
   redirectToCheckout = async () => {
     const { error } = await this.stripe.redirectToCheckout({
+      items: [
+        {
+          plan:
+            process.env.NODE_ENV === 'production'
+              ? 'plan_FA1maWmEMVsFUe'
+              : 'plan_EbzcEa5yCpjaMM',
+          quantity: 1,
+        },
+      ],
       successUrl: 'https://lovenbakeshop.com/thank_you/',
       cancelUrl: 'https://lovenbakeshop.com/subscribe/',
     });
