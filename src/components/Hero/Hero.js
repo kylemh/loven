f7a70ef49b9c7a2bb8a16c60405487490a62bc8f
Allen Anthes <abanthes@gmail.com>
2019-06-09 09:40:04 -0500

    Pull Hero content into separate component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import BackgroundImage from 'gatsby-background-image';
+import { graphql, useStaticQuery } from 'gatsby';
+import Logo from '../../images/loven-logo.svg';
+import { LinkButton } from '../Button';
+import styles from './Hero.module.scss'
+
+const Hero = props => {
+  const content = {
+    callToAction: { callToAction: "Spread some L'oven:\nSubscribe to our Bakery Boxes!" },
+    callToActionButtonText: 'Subscribe!',
+  };
+  const { callToAction } = content.callToAction;
+  const { callToActionButtonText } = content;
+
+
+  const { backgroundImg } = useStaticQuery(graphql`
+    query {
+        backgroundImg: file(relativePath: { eq: "overhead-3-pies.jpg" }) {
+            sharp: childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+            }
+        }
+
+#      homePageContent: allContentfulHomePage {
+#        nodes {
+#          callToAction {
+#            callToAction
+#          }
+#          callToActionButtonText
+#        }
+#      }
+    }
+  `);
+  return (
+    <BackgroundImage
+      className={styles.Hero}
+      style={{
+        height: '100vh',
+      }}
+      fluid={backgroundImg.sharp.fluid}
+    >
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <img alt="L'oven Logo" src={Logo} className={styles.logo} />
+
+          <p className={styles.ctaParagraph}>{callToAction}</p>
+
+          <LinkButton to="/subscribe">{callToActionButtonText}</LinkButton>
+        </div>
+      </div>
+    </BackgroundImage>
+  );
+};
+
+export default Hero;
