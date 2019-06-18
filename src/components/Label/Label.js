aaeaa4f813be6233e6ccc72bcd809da0a39e4628
Kyle Holmberg <kyle.holmberg@nike.com>
2019-06-18 00:24:30 -0700

    Add Label component

---
+++ src/components/Label/Label.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { node, string, bool } from 'prop-types';
+import classNames from 'classnames';
+import ScreenReaderOnly from '../ScreenReaderOnly';
+import styles from './Label.scss';
+
+Label.propTypes = {
+  children: node.isRequired,
+  className: string,
+  for: string.isRequired,
+  isHidden: bool, // visually hides the label, but maintains accessibility
+};
+
+Label.defaultProps = {
+  className: undefined,
+  isHidden: false,
+};
+
+function Label({ children, className, isHidden, ...props }) {
+  const TheLabel = (
+    <label
+      // for isnt destructured because it's a reserved word in JavaScript
+      // eslint-disable-next-line react/destructuring-assignment
+      htmlFor={props.for}
+      className={classNames(styles.Label, className)}
+    >
+      {children}
+    </label>
+  );
+
+  return isHidden ? <ScreenReaderOnly>{TheLabel}</ScreenReaderOnly> : <>{TheLabel}</>;
+}
+
+export default Label;
